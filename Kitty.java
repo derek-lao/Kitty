82cd159c5580fa75a69731e9a685887a715c3773
derek-lao <43069580+derek-lao@users.noreply.github.com>
2018-10-03 10:10:40 -0400

    Add files via upload

---
+++ Kitty.java
@@ -0,0 +1,34 @@
+public class Kitty{
+	private int age;
+	private String name;
+	
+	public Kitty(int startAge, String startName){
+		age=startAge;
+		name=startName;
+	}
+	
+	public int getAge(int age){
+		return age;
+	}
+	
+	public String getName(String name){
+		return name;
+	}
+
+	public String toString(String name){
+		return name;
+	}
+
+	public String changeName(String newName){
+		name=newName;
+		return name;
+	}
+
+	public int makeOlder(int newAge){
+		age=newAge;
+		return age;
+	}
+	
+	public 
+	
+	
