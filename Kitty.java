dc8dcd2d0d042da1ad72137e06bda254b7149f89
Derek Lao <dlao00@stuy.edu>
2018-10-03 16:03:05 -0400

    finished Kitty.java stuff

--- Kitty.java
+++ Kitty.java
@@ -1,34 +1,32 @@
 public class Kitty{
 	private int age;
 	private String name;
-	
-	public Kitty(int startAge, String startName){
-		age=startAge;
-		name=startName;
-	}
-	
-	public int getAge(int age){
-		return age;
-	}
-	
-	public String getName(String name){
-		return name;
-	}
 
-	public String toString(String name){
-		return name;
-	}
+		public Kitty(int startAge, String startName){
+			age=startAge;
+			name=startName;
+		}
 
-	public String changeName(String newName){
-		name=newName;
-		return name;
-	}
+		public int getAge(int age){
+			return age;
+		}
 
-	public int makeOlder(int newAge){
-		age=newAge;
-		return age;
-	}
-	
-	public 
-	
-	
+		public String getName(String name){
+			return name;
+		}
+
+		public String toString(String name){
+			return name;
+		}
+
+		public String changeName(String newName){
+			name=newName;
+			return name;
+		}
+
+		public int makeOlder(int newAge){
+			age=newAge;
+			return age;
+		}
+
+}
