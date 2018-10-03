c359b91b08a3c7219366535e45400fd5374379f1
Derek Lao <dlao00@stuy.edu>
2018-10-03 16:22:31 -0400

    finished kitty.java for driver.java

--- Driver.java
+++ Driver.java
@@ -16,14 +16,14 @@ public class Driver{
 		//should print "Katya is 5 years old."
 
 		System.out.println(a);
-		//should print Katya. This is an implied call to toString()
+		//should print The amazing Katya. This is an implied call to toString()
 
 		Kitty  b =  new Kitty();
-		//2nd constructor makes a Kitty named mittens that is 2 years old
+		//2nd constructor makes a Kitty named Mittens that is 2 years old
 		//this constructor takes no parameters.
 
 		System.out.println(b);
-		//should print Mittens. This is an implied call to toString()
+		//should print The amazing Mittens. This is an implied call to toString()
 
 		System.out.println(b.getName() + " is " + b.getAge() + " years old.");
 		//should print "Mittens is 2 years old."
--- Kitty.java
+++ Kitty.java
@@ -2,31 +2,34 @@ public class Kitty{
 	private int age;
 	private String name;
 
-		public Kitty(int startAge, String startName){
+		public Kitty(String startName, int startAge){
 			age=startAge;
 			name=startName;
 		}
 
-		public int getAge(int age){
+		public Kitty(){
+			name="Mittens";
+			age=2;
+		}
+
+		public int getAge(){
 			return age;
 		}
 
-		public String getName(String name){
+		public String getName(){
 			return name;
 		}
 
-		public String toString(String name){
-			return name;
+		public String toString(){
+			return "The amazing " + name;
 		}
 
-		public String changeName(String newName){
+		public void changeName(String newName){
 			name=newName;
-			return name;
 		}
 
-		public int makeOlder(int newAge){
-			age=newAge;
-			return age;
+		public void makeOlder(){
+			age++;
 		}
 
 }
